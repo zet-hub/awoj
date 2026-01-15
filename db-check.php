f7dbce1772ff529b25d541f8686566d32cc706b0
zet-hub <fitzger.cascara@gmail.com>
2026-01-15 13:49:27 +0800

    test

--- db-check.php
+++ db-check.php
@@ -5,3 +5,4 @@ $stmt = $pdo->prepare("SELECT id, email, role FROM users WHERE email = :email");
 $stmt->execute(['email' => 'admin@example.com']);
 $user = $stmt->fetch();
 var_dump($user);
+//the end
